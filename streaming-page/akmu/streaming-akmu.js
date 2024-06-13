1cc79a8a6a607b9346bee8067de5c618da63710f
y-ngm-n <wogitopp@naver.com>
2024-06-13 14:40:40 +0900

    streaming-page 수정

---
+++ streaming-page/akmu/streaming-akmu.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const lp = document.querySelector('.lp');
+  const audio = new Audio('../../musics/akmu.mp3');
+  let isRotating = false;
+  let rotation = 0;
+  let lastTimestamp = null;
+
+  function animate(timestamp) {
+      if (!lastTimestamp) {
+          lastTimestamp = timestamp;
+      }
+
+      const delta = timestamp - lastTimestamp;
+
+      if (isRotating) {
+          rotation += delta * 0.09;
+          rotation %= 360;
+          lp.style.transform = `rotate(${rotation}deg)`;
+      }
+
+      lastTimestamp = timestamp;
+      requestAnimationFrame(animate);
+  }
+
+  lp.addEventListener('click', () => {
+      isRotating = !isRotating;
+      if (isRotating) {
+          audio.play();
+          lastTimestamp = null;
+          requestAnimationFrame(animate);
+      } else {
+          audio.pause();
+      }
+  });
+});
